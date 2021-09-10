79f9bee552f3a1c50259820cba8feefa340808d7
Taras Shpek <taras.shpek@gmail.com>
2021-09-10 18:43:41 +0300

    make TestJwtProperties for all profiles

--- src/main/java/software/plusminus/jwt/config/TestJwtProperties.java
+++ src/main/java/software/plusminus/jwt/config/TestJwtProperties.java
@@ -3,7 +3,6 @@ package software.plusminus.jwt.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
@@ -11,7 +10,6 @@ import javax.annotation.PostConstruct;
 @Configuration
 @ConfigurationProperties("jwt")
 @Primary
-@Profile("test")
 public class TestJwtProperties extends JwtProperties {
     
     @PostConstruct
